42b2f90700d3865e5b42249679d01d4005a49dd2
Sriram s <123556950+SriRam-Gdev@users.noreply.github.com>
2026-08-18 19:01:54 +0530

    Testing AFP

--- Design Patterns and SOLID Principles/Program.cs
+++ Design Patterns and SOLID Principles/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿using src.Concepts.DesignPatterns.Creational.AbstractFactory;
+using src.Concepts.DesignPatterns.Creational.AbstractFactory.Windows;
+var UserSettingsFormUserSettingsForm = new UserSettingsForm();
+
+UserSettingsFormUserSettingsForm.Render(new WindowsUIComponentFactory());
--- Design Patterns and SOLID Principles/obj/Debug/net10.0/MyConsoleApp.AssemblyInfo.cs
+++ Design Patterns and SOLID Principles/obj/Debug/net10.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4402254c125269278e033a641a344c9c49b1d299")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cacaa1890e9c2716a2a8220c6f8e9e07bc5cc1bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
